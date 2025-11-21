ac2cfa0e0836057e7ff4bcf1927d17f1f216ae18
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-11-21 07:51:49 -0500

    configuraciones de admins / credenciales para cada modelo

--- operaciones/admin.py
+++ operaciones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import OperacionComercial
 
-# Register your models here.
+
+@admin.register(OperacionComercial)
+class OperacionComercialAdmin(admin.ModelAdmin):
+    list_display = ('tipo_operacion', 'empresa', 'producto', 'cantidad', 'estado', 'incoterm', 'fecha_creacion')
+    list_filter = ('tipo_operacion', 'estado', 'incoterm')
+    search_fields = ('empresa__user__username', 'producto__nombre')
--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Producto
 
-# Register your models here.
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'productor', 'precio_unitario', 'stock', 'pais_origen', 'fecha_registro')
+    list_filter = ('pais_origen',)
+    search_fields = ('nombre', 'productor__user__username')
