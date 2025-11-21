3d274ceee7e60d95e0f22830b8f672c3237f5717
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-11-21 10:36:35 -0500

    templates de productos mejorada , esto es para guia para el desarrollo luego en el frontend sera mejorado

--- productos/urls.py
+++ productos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import lista_productos
+from .views import lista_productos, detalle_producto
 
 urlpatterns = [
     path('', lista_productos, name='lista_productos'),
+    path('producto/<int:pk>/', detalle_producto, name='detalle_producto'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Producto
 
 def lista_productos(request):
     productos = Producto.objects.all()
     return render(request, 'productos/lista.html', {'productos': productos})
+
+def detalle_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, 'productos/detalle.html', {'producto': producto})
