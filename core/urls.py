eb5b9f472fd6279662311c20479cffba88cda20f
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-11-21 10:16:43 -0500

    agregando una vista en html simple para verificar que se conecta el backend con el frontend

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import RedirectView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/admin/', permanent=False)),  # redireccion para admin directo (solo para el desarrollo de la web)
+    #path('', RedirectView.as_view(url='/admin/', permanent=False)),  # redireccion para admin directo (solo para el desarrollo de la web)
+    path('', include('productos.urls')),
 ]
